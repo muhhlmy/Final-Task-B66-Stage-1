469a2dc1f838344ccd17abf0d65e7d5c83452257
muhhlmy <muhhlmy@gmail.com>
2026-04-18 11:48:17 +0700

    Add tech stack section and enhance styling; include Font Awesome and custom animations

--- app.js
+++ app.js
@@ -13,10 +13,69 @@ app.use(express.static("public"));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
+// Data Tech Stack
+const techStack = [
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-html5",
+    iconClass: "text-[#E34F26]",
+    iconName: "HTML",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-css3-alt",
+    iconClass: "text-[#204CE6]",
+    iconName: "CSS",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-js",
+    iconClass: "text-[#F0DB4F]",
+    iconName: "JavaScript",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-python",
+    iconClass: "text-[#3D74A3]",
+    iconName: "Python",
+  },
+  {
+    iconPrefix: "",
+    icon: "devicon-tailwindcss-original",
+    iconClass: "colored",
+    iconName: "Tailwind",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-vuejs",
+    iconClass: "text-green-500",
+    iconName: "Vue.js",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-bootstrap",
+    iconClass: "text-purple-600",
+    iconName: "Bootstrap",
+  },
+  {
+    iconPrefix: "fa-brands",
+    icon: "fa-node-js",
+    iconClass: "text-green-600",
+    iconName: "Node.js",
+  },
+  {
+    iconPrefix: "fa-solid",
+    icon: "fa-server",
+    iconClass: "text-gray-700",
+    iconName: "Express.js",
+  },
+];
+
 // Routes
 app.get("/", (req, res) => {
   res.render("home", {
     title: "Portofolio | Muhammad Helmy",
+    techStack: techStack,
   });
 });
 
